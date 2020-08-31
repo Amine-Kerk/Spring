f7f56a61ee1676216a702070786daa68ab7a63b7
Amine KERKOUB <kerk.amine@gmail.com>
2020-08-31 15:07:17 +0200

    TP 1.1 configuration daoMemoire et daoFichier

---
+++ src/main/java/dev/AppSpringXML.java
@@ -0,0 +1,36 @@
+package dev;
+
+import java.util.Scanner;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import dev.ihm.Menu;
+
+
+
+public class AppSpringXML {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-config-memoire.xml");
+		  ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-config-fichier.xml");
+		
+		
+		
+		
+		// récupération du bean Menu
+		  Menu menu = context.getBean(Menu.class);
+		  menu.afficher();
+		  // fermeture du Scanner
+		  context.getBean(Scanner.class).close();
+		  // fermeture du contexte Spring
+		  context.close();
+
+		
+		  
+		
+		
+		
+	}
+
+}
